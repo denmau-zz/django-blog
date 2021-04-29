7ee2bc6979bd09ee2d113ea0c292d45c1b54b1f6
Denmau <dmkamau475@gmail.com>
2021-04-29 13:42:19 +0300

    project milestone complete

--- day-2-django-blog/blog/tests.py
+++ day-2-django-blog/blog/tests.py
@@ -25,13 +25,11 @@ class BlogTests(TestCase):
 
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
-
         self.assertEqual(f'{self.post.author}', 'testuser')
         self.assertEqual(f'{self.post.body}', 'Nice body content')
 
     def test_post_list_view(self):
         response = self.client.get(reverse('home'))
-
         self.assertEqual(response.status_code, 200)
 
     def test_home_page_status_code(self):
@@ -47,3 +45,29 @@ class BlogTests(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response, 'A good title')
         self.assertTemplateUsed(response, 'post_detail.html')
+
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'Test title',
+            'body': 'Test text',
+            'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'Test title')
+        self.assertEqual(Post.objects.last().body, 'Test text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated text',
+        })
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_post_delete_view(self):
+        response = self.client.post(
+            reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
--- day-2-django-blog/blog/views.py
+++ day-2-django-blog/blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from django.views.generic import DetailView
+# We use reverse_lazy as opposed to just reverse so that it won’t execute the URL redirect
+# until our view has finished deleting the blog post.
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from blog.models import Post
 
@@ -14,3 +16,20 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
