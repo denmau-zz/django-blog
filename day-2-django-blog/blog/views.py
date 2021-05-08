17c86da8f826ac225a101ae4dd92d4b500e750d1
Denmau <dmkamau475@gmail.com>
2021-05-08 18:29:19 +0300

    blog complete

--- day-2-django-blog/blog/views.py
+++ day-2-django-blog/blog/views.py
@@ -1,10 +1,11 @@
 # We use reverse_lazy as opposed to just reverse so that it won’t execute the URL redirect
 # until our view has finished deleting the blog post.
+from abc import ABC
+
 from django.contrib import auth
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from django.urls import reverse_lazy, reverse
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -22,7 +23,7 @@ class BlogDetailView(DetailView):
     template_name = 'post_detail.html'
 
 
-class BlogCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'body']
@@ -54,25 +55,16 @@ class BlogDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return obj.author == self.request.user
 
 
-# A logged in user may add a new comment from the post_detail page
-# The comment is captured in a form and sent to this view(mapped from url) using POST method
-# This view is responsible for reading the POST details and creating a new Comment object from Comment model
-# after this it returns the template that contains details about the post
-# which is similar to refreshing the page, and thus the new comment is now visible
+# A logged in user may add a new comment from the post_detail page The comment is captured in a form and sent to this
+# view(mapped from url) using POST method This view is responsible for reading the POST details and creating a new
+# Comment object using Comment model after this it returns back to the post details page, which is similar to
+# refreshing the page, and thus the new comment is now visible
 
 
 class CreateComment(View):
     def post(self, request, pk):
-        received_comment = request.POST['comment']
-        received_pk = Post.objects.get(id=pk)
-        # instantiate new comment
+        # instantiate new comment with details from POST
         Comment.objects.create(comment=request.POST['comment'], post=Post(pk), author=auth.get_user(request))
 
-        # print(f"Here is the PK: {received_pk}; and the request is {received_comment}")
-        # return render(request, 'test.html', {'my_message': f"PK:{received_pk} : Message:{received_comment}"})
-        return redirect('post_detail', pk)
-        # Display POST parameters - for testing purposes
-        # return render(request, 'test.html', {
-        #     'my_message': {request.POST['comment']},
-        # })
         # go back to post details
+        return redirect('post_detail', pk)
